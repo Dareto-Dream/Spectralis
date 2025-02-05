6729067b84327dadd8dd0307c3d0baff281ff3d2
dareto-dream <dare.to.0ream@gmail.com>
2025-02-05 15:45:25 -0800

    looks ok

--- Spectralis/Form1.cs
+++ Spectralis/Form1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace Spectralis
@@ -8,6 +9,39 @@ namespace Spectralis
         public Form1()
         {
             InitializeComponent();
+            SetupEventHandlers();
         }
+
+        private void SetupEventHandlers()
+        {
+            openToolStripMenuItem.Click += OnOpenFile;
+            btnPlay.Click += OnPlay;
+            btnPause.Click += OnPause;
+            btnStop.Click += OnStop;
+            btnPrev.Click += OnPrev;
+            btnNext.Click += OnNext;
+            progressBar.Scroll += OnProgressScroll;
+            volumeBar.Scroll += OnVolumeScroll;
+        }
+
+        private void OnOpenFile(object sender, EventArgs e)
+        {
+            using var dlg = new OpenFileDialog
+            {
+                Filter = "Audio Files|*.mp3;*.flac;*.wav;*.ogg;*.opus;*.m4a;*.aac;*.mid|All Files|*.*",
+                Multiselect = true
+            };
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+            }
+        }
+
+        private void OnPlay(object sender, EventArgs e) { }
+        private void OnPause(object sender, EventArgs e) { }
+        private void OnStop(object sender, EventArgs e) { }
+        private void OnPrev(object sender, EventArgs e) { }
+        private void OnNext(object sender, EventArgs e) { }
+        private void OnProgressScroll(object sender, EventArgs e) { }
+        private void OnVolumeScroll(object sender, EventArgs e) { }
     }
 }
