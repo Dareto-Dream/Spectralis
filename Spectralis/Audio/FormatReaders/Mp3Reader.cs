14e4d519e27025447c7c01e7a13458365e78cd44
dareto-dream <dare.to.0ream@gmail.com>
2025-03-24 09:00:00 -0800

    nvm

--- Spectralis/Audio/FormatReaders/Mp3Reader.cs
+++ Spectralis/Audio/FormatReaders/Mp3Reader.cs
@@ -6,10 +6,12 @@ namespace Spectralis.Audio.FormatReaders
     public class Mp3Reader : IAudioReader
     {
         private readonly Mp3FileReader _inner;
+        private readonly Mp3WaveFormat _mp3Format;
 
         public Mp3Reader(string filePath)
         {
             _inner = new Mp3FileReader(filePath);
+            _mp3Format = _inner.Mp3WaveFormat;
         }
 
         public WaveFormat WaveFormat => _inner.WaveFormat;
@@ -19,13 +21,19 @@ namespace Spectralis.Audio.FormatReaders
         public TimeSpan Position
         {
             get => _inner.CurrentTime;
-            set => _inner.CurrentTime = value;
+            set
+            {
+                var clamped = TimeSpan.FromSeconds(Math.Max(0, Math.Min(value.TotalSeconds, _inner.TotalTime.TotalSeconds)));
+                _inner.CurrentTime = clamped;
+            }
         }
 
         public int Read(byte[] buffer, int offset, int count)
         {
             return _inner.Read(buffer, offset, count);
         }
+
+        public int Bitrate => _mp3Format?.AverageBytesPerSecond * 8 / 1000 ?? 0;
 
         public void Dispose() => _inner?.Dispose();
     }
