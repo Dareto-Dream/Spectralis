211d82a9b9abb712745606f3d1dc6f367c06c1d4
dareto-dream <dare.to.0ream@gmail.com>
2025-08-12 15:45:00 -1000

    ui drop

---
+++ Spectralis.App/Views/QueueView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Spectralis.App.Views
+{
+    public partial class QueueView : UserControl
+    {
+        public QueueView()
+        {
+            InitializeComponent();
+        }
+    }
+}
