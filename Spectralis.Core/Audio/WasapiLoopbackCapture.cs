c9d3d5ec8a1586866bc3c1a9f3590f729369d632
dareto-dream <dare.to.0ream@gmail.com>
2025-11-24 11:00:00 -1000

    fixed the thing

--- Spectralis.Core/Audio/WasapiLoopbackCapture.cs
+++ Spectralis.Core/Audio/WasapiLoopbackCapture.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Runtime.InteropServices;
+using NAudio.CoreAudioApi;
 using NAudio.Wave;
 
 namespace Spectralis.Core.Audio
@@ -7,6 +8,8 @@ namespace Spectralis.Core.Audio
     public class WasapiLoopbackCaptureAdapter : IAudioCapture
     {
         private NAudio.CoreAudioApi.WasapiLoopbackCapture? _capture;
+        private MMDeviceEnumerator? _enumerator;
+        private MMDevice? _device;
         private bool _disposed;
         private bool _isCapturing;
 
@@ -24,7 +27,23 @@ namespace Spectralis.Core.Audio
 
         public void StartCapture()
         {
-            _capture = new NAudio.CoreAudioApi.WasapiLoopbackCapture();
+            DisposeCapture();
+
+            _enumerator = new MMDeviceEnumerator();
+            try
+            {
+                _device = _enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+                _capture = new NAudio.CoreAudioApi.WasapiLoopbackCapture(_device);
+            }
+            catch
+            {
+                _enumerator.Dispose();
+                _enumerator = null;
+                _device?.Dispose();
+                _device = null;
+                throw;
+            }
+
             _capture.DataAvailable += (s, e) => DataAvailable?.Invoke(this, e);
             _capture.RecordingStopped += (s, e) =>
             {
@@ -39,13 +58,24 @@ namespace Spectralis.Core.Audio
         {
             _capture?.StopRecording();
             _isCapturing = false;
+            DisposeCapture();
+        }
+
+        private void DisposeCapture()
+        {
+            _capture?.Dispose();
+            _capture = null;
+            _device?.Dispose();
+            _device = null;
+            _enumerator?.Dispose();
+            _enumerator = null;
         }
 
         public void Dispose()
         {
             if (_disposed) return;
             _disposed = true;
-            _capture?.Dispose();
+            DisposeCapture();
         }
     }
 }
