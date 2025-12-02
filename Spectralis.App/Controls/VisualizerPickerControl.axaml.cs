bbc52dde306e4b835b53482c0ff64b453581d551
dareto-dream <dare.to.0ream@gmail.com>
2025-12-02 10:00:00 -1000

    control added

---
+++ Spectralis.App/Controls/VisualizerPickerControl.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Spectralis.App.Controls
+{
+    public partial class VisualizerPickerControl : UserControl
+    {
+        public VisualizerPickerControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
