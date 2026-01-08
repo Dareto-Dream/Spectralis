c0fa1a708957a635745bac136df94fcc1ae093c9
dareto-dream <dare.to.0ream@gmail.com>
2026-01-08 11:44:12 -1000

    dialog appeared

---
+++ Spectralis.App/Views/ListeningHistoryView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Spectralis.App.Views
+{
+    public partial class ListeningHistoryView : UserControl
+    {
+        public ListeningHistoryView()
+        {
+            InitializeComponent();
+        }
+    }
+}
