78c7275e686be37ea50e7343bbf25c1757a03964
dareto-dream <dare.to.0ream@gmail.com>
2026-01-21 09:55:37 -1000

    ui drop

---
+++ Spectralis.App/Controls/BeatGridView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Spectralis.App.Controls
+{
+    public partial class BeatGridView : UserControl
+    {
+        public BeatGridView()
+        {
+            InitializeComponent();
+        }
+    }
+}
