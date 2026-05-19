f7a91ef79499eb23f39669f10888e4d6e3a88111
dareto-dream <dare.to.0ream@gmail.com>
2026-05-19 11:00:00 -0700

    chat panel

---
+++ Spectralis.App/Controls/SharedPlayChatPanel.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Spectralis.App.Controls
+{
+    public partial class SharedPlayChatPanel : UserControl
+    {
+        public SharedPlayChatPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
