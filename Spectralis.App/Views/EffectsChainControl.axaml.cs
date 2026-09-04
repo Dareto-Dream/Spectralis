4e1af3a5c44d55c18e8bc1006804d479dfeaf302
dareto-dream <dare.to.0ream@gmail.com>
2026-09-04 13:06:00 -1000

    eq finally looks like an envelope

--- Spectralis.App/Views/EffectsChainControl.axaml.cs
+++ Spectralis.App/Views/EffectsChainControl.axaml.cs
@@ -13,7 +13,21 @@ public partial class EffectsChainControl : UserControl
 
     private EffectsChainViewModel? ViewModel => DataContext as EffectsChainViewModel;
 
+    private EqEditorViewModel? EqEditor => ViewModel?.SelectedEffect?.EqEditor;
+
     private void OnAddEffect(object? sender, RoutedEventArgs e) => ViewModel?.AddSelectedEffect();
+
+    private void OnEqReset(object? sender, RoutedEventArgs e) => EqEditor?.ResetToFlat();
+
+    private void OnEqSavePreset(object? sender, RoutedEventArgs e)
+    {
+        if (EqEditor is { } editor)
+        {
+            editor.SaveCurrentAsPreset(editor.NewPresetName);
+        }
+    }
+
+    private void OnEqDeletePreset(object? sender, RoutedEventArgs e) => EqEditor?.DeleteSelectedPreset();
 
     private void OnMoveUp(object? sender, RoutedEventArgs e) => ViewModel?.MoveSelectedEffectUp();
 
