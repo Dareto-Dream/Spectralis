c5a20a7571a7b27f421ab34663a9e46fa1b44df8
dareto-dream <dare.to.0ream@gmail.com>
2026-09-08 11:00:36 -1000

    podcast icon

--- Spectralis.App/Design/IconData.cs
+++ Spectralis.App/Design/IconData.cs
@@ -15,6 +15,7 @@ public static class IconData
     public const string Settings = "M4,6 H13 V8 H4 Z M15,4 H19 V10 H15 Z M11,11 H20 V13 H11 Z M5,9 H9 V15 H5 Z M4,17 H13 V19 H4 Z M15,15 H19 V21 H15 Z";
     public const string Obs = "M3,5 H21 V19 H3 Z M13,11 H20 V17 H13 Z";
     public const string SongWars = "M7,4 H17 V10 H7 Z M10,10 H14 V15 H10 Z M6,15 H18 V18 H6 Z";
+    public const string Podcast = "M10,3 H14 V13 H10 Z M6,10 H8 C8,13 9.8,15 12,15 C14.2,15 16,13 16,10 H18 C18,14 15.4,16.6 13,17 V21 H11 V17 C8.6,16.6 6,14 6,10 Z";
 
     public const string Play = "M8,5 L20,12 L8,19 Z";
     public const string Pause = "M7,5 H11 V19 H7 Z M15,5 H19 V19 H15 Z";
